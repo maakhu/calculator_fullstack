3cac2dbc9c29f1ba843c7a0112efd6b18a4dbe30
maakhu <egri@me.com>
2023-06-21 19:01:58 +0200

    add design work

--- frontend/src/components/pages/Calculator.tsx
+++ frontend/src/components/pages/Calculator.tsx
@@ -11,34 +11,34 @@ export default function Calculator() {
       <h4>by Máté Egri</h4>
       <div className="calculator">
         <div className="display">
-          <div className="enteredNumber">0</div>
-          <div className="displayText">0</div>
+          <div className="curr-operation">0</div>
+          <div className="enteredNo">0</div>
         </div>
-        <div className="calculatorButtons">
+        <div className="buttonRow">
           <button>C</button>
           <button>M+</button>
           <button>M</button>
           <button>+</button>
         </div>
-        <div className="calculatorButtons">
+        <div className="buttonRow">
           <button>1</button>
           <button>2</button>
           <button>3</button>
           <button>-</button>
         </div>
-        <div className="calculatorButtons">
+        <div className="buttonRow">
           <button>4</button>
           <button>5</button>
           <button>6</button>
           <button>*</button>
         </div>
-        <div className="calculatorButtons">
+        <div className="buttonRow">
           <button>7</button>
           <button>8</button>
           <button>9</button>
           <button>/</button>
         </div>
-        <div className="calculatorButtons">
+        <div className="buttonRow">
           <button>,</button>
           <button>0</button>
           <button>DEL</button>
