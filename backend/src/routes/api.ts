aec1e0b83da99d66c55e9641e2a571d9b809273b
maakhu <egri@me.com>
2023-06-21 15:52:19 +0200

    create react app

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,21 +1,20 @@
-import express from 'express';
-import status from 'http-status';
-import apiErrorHandler from '../middlewares/apiErrorHandler';
-import * as calculatorController from '../controllers/calculatorController';
-import { HttpError, NotFoundError, ParameterError } from '../errors';
+import express from "express";
+import status from "http-status";
+import apiErrorHandler from "../middlewares/apiErrorHandler";
+import * as calculatorController from "../controllers/calculatorController";
+import { HttpError, NotFoundError, ParameterError } from "../errors";
 
 const apiRouter = express.Router();
 apiRouter.use(express.json());
 
-apiRouter.get('/memory', calculatorController.getMemory);
-apiRouter.post('/memory', calculatorController.postMemory);
-apiRouter.put('/memory', calculatorController.putMemory);
-apiRouter.delete('/memory', calculatorController.deleteMemory);
+apiRouter.get("/memory", calculatorController.getMemory);
+apiRouter.post("/memory", calculatorController.postMemory);
+apiRouter.put("/memory", calculatorController.putMemory);
+apiRouter.delete("/memory", calculatorController.deleteMemory);
 
-apiRouter.use('*', (req, res, next) => {
-    next(new NotFoundError('API endpoint not found'));
-}
-);
+apiRouter.use("*", (req, res, next) => {
+  next(new NotFoundError("API endpoint not found"));
+});
 apiRouter.use(apiErrorHandler);
 
 export default apiRouter;
