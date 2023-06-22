b7197c61f961b3c2dc455df6b24be546f8ef9fff
maakhu <egri@me.com>
2023-06-22 17:00:19 +0200

    change display to component & update css files

--- frontend/src/components/Calculator/Calculator.tsx
+++ frontend/src/components/Calculator/Calculator.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useState } from "react";
 import Display from "../Display/Display";
-import { Operator } from '../../lib/types'
+import { Digit, Operator } from '../../lib/types'
 import "./Calculator.css";
 
 export const Calculator: FunctionComponent = () => {
@@ -9,6 +9,144 @@ export const Calculator: FunctionComponent = () => {
   const [waitingForOperand, setWaitingForOperand] = useState<boolean>(true)
   const [pendingOperator, setPendingOperator] = useState<Operator>()
   const [display, setDisplay] = useState<string>('0')
+
+  const calculate = (rightOperand: number, pendingOperator: Operator): boolean => {
+    let newResult = result
+
+    switch (pendingOperator) {
+      case '+':
+        newResult += rightOperand
+        break
+      case '-':
+        newResult -= rightOperand
+        break
+      case '×':
+        newResult *= rightOperand
+        break
+      case '÷':
+        if (rightOperand === 0) {
+          return false
+        }
+
+        newResult /= rightOperand
+    }
+
+    setResult(newResult)
+    setDisplay(newResult.toString().toString().slice(0, 12))
+
+    return true
+  }
+
+  const onDigitButtonClick = (digit: Digit) => {
+    let newDisplay = display
+
+    if ((display === '0' && digit === 0) || display.length > 12) {
+      return
+    }
+
+    if (waitingForOperand) {
+      newDisplay = ''
+      setWaitingForOperand(false)
+    }
+
+    if (display !== '0') {
+      newDisplay = newDisplay + digit.toString()
+    } else {
+      newDisplay = digit.toString()
+    }
+
+    setDisplay(newDisplay)
+  }
+
+  const onPointButtonClick = () => {
+    let newDisplay = display
+
+    if (waitingForOperand) {
+      newDisplay = '0'
+    }
+
+    if (newDisplay.indexOf('.') === -1) {
+      newDisplay = newDisplay + '.'
+    }
+
+    setDisplay(newDisplay)
+    setWaitingForOperand(false)
+  }
+
+  const onOperatorButtonClick = (operator: Operator) => {
+    const operand = Number(display)
+
+    if (typeof pendingOperator !== 'undefined' && !waitingForOperand) {
+      if (!calculate(operand, pendingOperator)) {
+        return
+      }
+    } else {
+      setResult(operand)
+    }
+
+    setPendingOperator(operator)
+    setWaitingForOperand(true)
+  }
+
+  const onChangeSignButtonClick = () => {
+    const value = Number(display)
+
+    if (value > 0) {
+      setDisplay('-' + display)
+    } else if (value < 0) {
+      setDisplay(display.slice(1))
+    }
+  }
+
+  const onEqualButtonClick = () => {
+    const operand = Number(display)
+
+    if (typeof pendingOperator !== 'undefined' && !waitingForOperand) {
+      if (!calculate(operand, pendingOperator)) {
+        return
+      }
+
+      setPendingOperator(undefined)
+    } else {
+      setDisplay(operand.toString())
+    }
+
+    setResult(operand)
+    setWaitingForOperand(true)
+  }
+
+  const onAllClearButtonClick = () => {
+    setMemory(0)
+    setResult(0)
+    setPendingOperator(undefined)
+    setDisplay('0')
+    setWaitingForOperand(true)
+  }
+
+  const onClearEntryButtonClick = () => {
+    setDisplay('0')
+    setWaitingForOperand(true)
+  }
+
+  const onMemoryRecallButtonClick = () => {
+    setDisplay(memory.toString())
+    setWaitingForOperand(true)
+  }
+
+  const onMemoryClearButtonClick = () => {
+    setMemory(0)
+    setWaitingForOperand(true)
+  }
+
+  const onMemoryPlusButtonClick = () => {
+    setMemory(memory + Number(display))
+    setWaitingForOperand(true)
+  }
+
+  const onMemoryMinusButtonClick = () => {
+    setMemory(memory - Number(display))
+    setWaitingForOperand(true)
+  }
 
   return (
     <div className="container">
--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,1 +1,2 @@
 export type Operator = '+' | '-' | '×' | '÷'
+export type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
